e5e9820fb08b7daa6c91d6019c3c2ea60f1fb603
jaehee <gjald36@gmail.com>
2023-06-22 16:32:19 +0900

    ㅎㅎ

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import homin from './homin2.gif';
-import homin3 from './homin.gif';
 
 function App() {
-
-  const homin2Background = `url(${homin3})`;
 
   return (
     <div className="App-logo2">
       <img alt='' src={homin} width={300} border="1"style={{marginTop:'170px'}}/>
-      <h2 style={{marginTop:'135px', background: homin2Background}}> Docker 배포 성공 기원!!!</h2>
+      <h2 style={{marginTop:'135px'}}> Docker 배포 성공 기원!!!</h2>
     </div>
   );
 }
