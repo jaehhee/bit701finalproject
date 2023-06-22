89d1e226129f151152a915b103832f02b3b9b5d3
jaehee <gjald36@gmail.com>
2023-06-22 12:22:31 +0900

    gradle

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import homin from './homin2.gif';
+import homin2 from './homin5.gif';
+import homin3 from './homin.gif';
 
 function App() {
+
+  const homin2Background = `url(${homin3})`;
+
   return (
-    <div className="App">
-      <img alt='' src={homin} width={300} border="1" className='App-logo' style={{marginTop:'170px'}}/>
-      <br></br>
-      <h2 style={{marginTop:'135px'}} className='App-logo' > Docker 배포 성공 기원!!!</h2>
+    <div>
+      <img alt='' src={homin} width={300} border="1"style={{marginTop:'170px'}} className="App-logo2"/>
+      <br/>
+      <h2 style={{marginTop:'135px', background: homin2Background}} className="App-logo2"> Docker 배포 성공 기원!!!</h2>
     </div>
   );
 }
